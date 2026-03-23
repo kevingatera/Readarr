9ba980540ba9d901cddf64f72f0d262294849f06
Kevin Gatera <kevingatera@gmail.com>
2026-03-23 12:27:25 -0400

    Include author in book lookup resources

--- src/Readarr.Api.V1/Books/BookLookupController.cs
+++ src/Readarr.Api.V1/Books/BookLookupController.cs
@@ -3,6 +3,8 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using NzbDrone.Core.MediaCover;
 using NzbDrone.Core.MetadataSource;
+using NzbDrone.Core.Organizer;
+using Readarr.Api.V1.Author;
 using Readarr.Http;
 
 namespace Readarr.Api.V1.Books
@@ -11,11 +13,13 @@ namespace Readarr.Api.V1.Books
     public class BookLookupController : Controller
     {
         private readonly ISearchForNewBook _searchProxy;
+        private readonly IBuildFileNames _fileNameBuilder;
         private readonly IMapCoversToLocal _coverMapper;
 
-        public BookLookupController(ISearchForNewBook searchProxy, IMapCoversToLocal coverMapper)
+        public BookLookupController(ISearchForNewBook searchProxy, IBuildFileNames fileNameBuilder, IMapCoversToLocal coverMapper)
         {
             _searchProxy = searchProxy;
+            _fileNameBuilder = fileNameBuilder;
             _coverMapper = coverMapper;
         }
 
@@ -31,15 +35,25 @@ namespace Readarr.Api.V1.Books
             foreach (var currentBook in books)
             {
                 var resource = currentBook.ToResource();
+                resource.Author = currentBook.Author.Value.ToResource();
 
                 _coverMapper.ConvertToLocalUrls(resource.Id, MediaCoverEntity.Book, resource.Images);
+                _coverMapper.ConvertToLocalUrls(resource.Author.Id, MediaCoverEntity.Author, resource.Author.Images);
 
                 var cover = resource.Images.FirstOrDefault(c => c.CoverType == MediaCoverTypes.Cover);
+                var poster = resource.Author.Images.FirstOrDefault(c => c.CoverType == MediaCoverTypes.Poster);
 
                 if (cover != null)
                 {
                     resource.RemoteCover = cover.RemoteUrl;
                 }
+
+                if (poster != null)
+                {
+                    resource.Author.RemotePoster = poster.RemoteUrl;
+                }
+
+                resource.Author.Folder = _fileNameBuilder.GetAuthorFolder(currentBook.Author);
 
                 yield return resource;
             }
